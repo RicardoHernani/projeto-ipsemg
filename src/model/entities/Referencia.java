3d6564c7e89483b305a1a98a04a608294b5889f6
RicardoHernani <ricardohernani@yahoo.com.br>
2020-07-28 12:04:09 -0300

    Referencia entity class

---
+++ src/model/entities/Referencia.java
@@ -0,0 +1,91 @@
+//Retirei os sets da classe Referencia para não haver mudanças acidentais
+
+package model.entities;
+
+import java.io.Serializable;
+
+public class Referencia implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer codigo;
+	private String procedimento;
+	private Double pontos;
+	private Double valor;
+	
+	public Referencia() {
+	}
+
+	public Referencia(Integer codigo, String procedimento, Double pontos, Double valor) {
+		this.codigo = codigo;
+		this.procedimento = procedimento;
+		this.pontos = pontos;
+		this.valor = valor;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public String getProcedimento() {
+		return procedimento;
+	}
+
+	public Double getPontos() {
+		return pontos;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		result = prime * result + ((pontos == null) ? 0 : pontos.hashCode());
+		result = prime * result + ((procedimento == null) ? 0 : procedimento.hashCode());
+		result = prime * result + ((valor == null) ? 0 : valor.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Referencia other = (Referencia) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		if (pontos == null) {
+			if (other.pontos != null)
+				return false;
+		} else if (!pontos.equals(other.pontos))
+			return false;
+		if (procedimento == null) {
+			if (other.procedimento != null)
+				return false;
+		} else if (!procedimento.equals(other.procedimento))
+			return false;
+		if (valor == null) {
+			if (other.valor != null)
+				return false;
+		} else if (!valor.equals(other.valor))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Referencia [codigo=" + codigo + ", procedimento=" + procedimento + ", pontos=" + pontos + ", valor="
+				+ valor + "]";
+	}
+	
+}
